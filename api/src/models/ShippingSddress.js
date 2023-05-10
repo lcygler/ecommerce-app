13c705303639bc516a573d71c10077124cfd0e51
ArrayagoM <juanmartinarrayago@gmail.com>
2023-05-10 09:53:55 -0300

    init table DB

---
+++ api/src/models/ShippingSddress.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+    sequelize.define('ShippingAddress', {
+        id:{
+            type:DataTypes.INTEGER,
+            autoIncrement:true,
+            primaryKey:true,
+        },
+        address:{
+            type:DataTypes.STRING
+        },
+        number:{
+            type:DataTypes.INTEGER
+        },
+        postCode:{
+            type:DataTypes.STRING
+        },
+        apartament: {
+            type: DataTypes.STRING
+        },
+        floor: {
+            type: DataTypes.INTEGER
+        },
+        home:{
+            type:DataTypes.STRING
+        },
+        city: {
+            type: DataTypes.STRING
+        },
+        state:{
+            type: DataTypes.STRING
+        },
+        country: {
+            type: DataTypes.STRING
+        }
+    })
+}
--- api/src/models/Usuario.js
+++ api/src/models/Usuario.js
@@ -15,6 +15,39 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true
+    },
+    lasname:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+    email:{
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique:true
+    },
+    password:{
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+    Birthdate:{
+      type: DataTypes.DATE,
+      allowNull: false
+    },
+    phone_Number:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    state:{
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    isadmin:{
+      type:DataTypes.BOOLEAN,
+      defaultValue: false
     }
   },{
     timestamps: true,
