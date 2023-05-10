0e51b92c3f32eb15d11208b461627c5cbd0ed908
lcygler <leandrocygler@gmail.com>
2023-05-10 18:52:07 -0300

    add landing component

--- client/src/views/Landing/Landing.jsx
+++ client/src/views/Landing/Landing.jsx
@@ -1,7 +1,19 @@
-import styles from "./Landing.module.css";
+import { useNavigate } from 'react-router-dom';
+import styles from './Landing.module.css';
 
 function Landing() {
-  return <div className={styles.container}></div>;
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/home');
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1>Bienvenido</h1>
+      <button onClick={handleClick}>Home</button>
+    </div>
+  );
 }
 
 export default Landing;
