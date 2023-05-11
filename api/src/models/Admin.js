0281b6e7c6f0da2ced9ac770a513f182a1519410
lcygler <leandrocygler@gmail.com>
2023-05-11 01:41:11 -0300

    fix variable names

--- api/src/models/Admin.js
+++ api/src/models/Admin.js
@@ -1,28 +1,28 @@
-const { DataTypes } =  require('sequelize');
+const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
-    sequelize.define('admin', {
-        id: {
-            type: DataTypes.UUID,
-            primaryKey:true,
-            defaultValue: DataTypes.UUIDV4,
-            allowNull: false
-        },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        lasName:{
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        password: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
-    })
-}
+  sequelize.define('Admin', {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastname: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
