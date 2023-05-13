ef1ce0d8ea65e915b914feadb17b2d429d1531a7
lcygler <leandrocygler@gmail.com>
2023-05-13 16:12:36 -0300

    client/redux: update redux config

--- client/src/redux/slices/index.js
+++
@@ -1,5 +0,0 @@
-import cartReducer from './cartSlice';
-import filterReducer from './filterSlice';
-import productReducer from './productSlice';
-
-export { cartReducer, filterReducer, productReducer };
