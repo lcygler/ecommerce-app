744ec2cc9cdba615dddb026bfd7579bbf1ac62ee
lcygler <leandrocygler@gmail.com>
2023-05-14 16:21:48 -0300

    client/components: create star rating

---
+++ client/src/components/StarRating.jsx
@@ -0,0 +1,23 @@
+import { Flex, Text } from '@chakra-ui/react';
+import { FaRegStar, FaStar, FaStarHalfAlt } from 'react-icons/fa';
+
+const StarRating = ({ value }) => {
+  const stars = [];
+  for (let i = 1; i <= 5; i++) {
+    if (i <= value) {
+      stars.push(<FaStar key={i} />);
+    } else if (i === Math.ceil(value) && !Number.isInteger(value)) {
+      stars.push(<FaStarHalfAlt key={i} />);
+    } else {
+      stars.push(<FaRegStar key={i} />);
+    }
+  }
+  return (
+    <Flex alignItems="center">
+      <Text mr="2">{value}</Text>
+      <Flex>{stars}</Flex>
+    </Flex>
+  );
+};
+
+export default StarRating;
