2bf9a2a68f260a8beb137ad2f3c367c3fe8de1a1
lcygler <leandrocygler@gmail.com>
2023-05-15 10:12:21 -0300

    api: update update catalog route

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,14 +2,13 @@ const { Router } = require('express');
 const usersRouter = require('./users.js');
 const categoryRoute = require('./CategoryRoute.js');
 const productRouter = require('./ProductRoute.js');
-const getCatalogHandler = require('../handlers/getCatalogHandler.js');
+const catalogRouter = require('./catalog.js');
 
 const router = Router();
 
 router.use('/users', usersRouter);
 router.use('/products', productRouter);
 router.use('/category', categoryRoute);
-
-router.use('/catalog', getCatalogHandler); //! temporalmente
+router.use('/catalog', catalogRouter); //! temporalmente
 
 module.exports = router;
