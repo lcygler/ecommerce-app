08ead8728b6703da4079e3c4b1f5c3d62a9e83a5
ArrayagoM <juanmartinarrayago@gmail.com>
2023-05-14 22:58:59 -0300

    Rutas Cartegory

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const usersRouter = require('./users.js');
+const categoryRoute = require('./CategoryRoute.js');
 const productRouter = require('./ProductRoute.js');
 
 const router = Router();
@@ -7,6 +8,6 @@ const router = Router();
 
 router.use('/users', usersRouter);
 router.use("/products", productRouter);
-
+router.use('/category', categoryRoute);
 
 module.exports = router;
