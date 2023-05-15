9f09ee74b170bd04fa0808e117cc4bdd99cdb3e7
ArrayagoM <juanmartinarrayago@gmail.com>
2023-05-15 00:01:48 -0300

    dialectOptions

--- api/src/db.js
+++ api/src/db.js
@@ -11,6 +11,9 @@ const sequelize = new Sequelize(dbUrl, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   dialectModule: pg,
+  dialectOptions: {
+    ssl: true
+  }
 });
 
 const basename = path.basename(__filename);
