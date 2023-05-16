be9ee6a3fb4e8e3c8b6cc8b80943bcd6a4439f8e
DieFleitas <diegofleitas98@outlook.es>
2023-05-15 23:19:01 -0300

    add trolley and cartdetail hasmany relation & cartdetail and product belongsto relation

--- api/src/db.js
+++ api/src/db.js
@@ -1,11 +1,14 @@
-require('dotenv').config();
-const pg = require('pg');
-const { Sequelize } = require('sequelize');
-const fs = require('fs');
-const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DB_DEPLOY } = process.env;
+require("dotenv").config();
+const pg = require("pg");
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DB_DEPLOY } =
+  process.env;
 
-const dbUrl = DB_DEPLOY || `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+const dbUrl =
+  DB_DEPLOY ||
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
 const sequelize = new Sequelize(dbUrl, {
   logging: false, // set to console.log to see the raw SQL queries
@@ -18,17 +21,23 @@ const basename = path.basename(__filename);
 const modelDefiners = [];
 
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, '/models'))
-  .filter((file) => file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js')
+fs.readdirSync(path.join(__dirname, "/models"))
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
   .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)));
+    modelDefiners.push(require(path.join(__dirname, "/models", file)));
   });
 
 // Injectamos la conexion (sequelize) a todos los modelos
 modelDefiners.forEach((model) => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
+let capsEntries = entries.map((entry) => [
+  entry[0][0].toUpperCase() + entry[0].slice(1),
+  entry[1],
+]);
 sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
@@ -57,38 +66,38 @@ Product.belongsTo(Admin);
 //Relacion entre user y review
 User.hasMany(Review);
 Review.belongsTo(User);
- //Relacion entre usueario y carrito
- User.hasMany(Trolley);
- Trolley.belongsTo(User);
- //Relacion usuario y su carritoDetail
+//Relacion entre usueario y carrito
+User.hasMany(Trolley);
+Trolley.belongsTo(User);
+//Relacion usuario y su carritoDetail
 User.hasMany(CartDetail);
 CartDetail.belongsTo(User);
 // detalle de la compra
 User.hasMany(PurchaseDetail);
 PurchaseDetail.belongsTo(User);
- //Relacion de usuario y su favoritos
- User.belongsToMany(Product, {through: "Favorites"});
- Product.belongsToMany(User, {through: "Favorites"});
- //Relacion entre Useario y Dirrecion
- User.hasMany(ShippingAddress);
- ShippingAddress.belongsTo(User);
- //Relacion entre producto y carrito
- Product.belongsToMany(Shopping, {through: 'Shopping_Product'});
- Shopping.belongsToMany(Product, {through: 'Shopping_Product'});
+//Relacion de usuario y su favoritos
+User.belongsToMany(Product, { through: "Favorites" });
+Product.belongsToMany(User, { through: "Favorites" });
+//Relacion entre Useario y Dirrecion
+User.hasMany(ShippingAddress);
+ShippingAddress.belongsTo(User);
+//Relacion entre producto y carrito
+Product.belongsToMany(Shopping, { through: "Shopping_Product" });
+Shopping.belongsToMany(Product, { through: "Shopping_Product" });
 // Product uno a muchos reviews;
 Product.hasMany(Review);
 Review.belongsTo(Product);
 //Relacion de Product a Categories M : M
-Product.belongsToMany(Category, {through: 'Product_Categorie'});
-Category.belongsToMany(Product, {through: 'Product_Categorie'});
+Product.belongsToMany(Category, { through: "Product_Categorie" });
+Category.belongsToMany(Product, { through: "Product_Categorie" });
 //Relacion entre Products y season M : M
-Product.belongsToMany(Season, {through: 'Product_Season'});
-Season.belongsToMany(Product, {through: 'Product_Season'});
+Product.belongsToMany(Season, { through: "Product_Season" });
+Season.belongsToMany(Product, { through: "Product_Season" });
 // Relación carrito y detalle del carrito
-Trolley.hasMany(CartDetail)
+Trolley.hasMany(CartDetail);
 CartDetail.belongsTo(Trolley);
 // Relación detalle del carrito y producto
-    CartDetail.belongsTo(Product);
+CartDetail.belongsTo(Product);
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
