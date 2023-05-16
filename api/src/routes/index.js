79d99132d7403e86680e031fad6498af9d04373c
lcygler <leandrocygler@gmail.com>
2023-05-16 15:54:17 -0300

    api: add seasons route

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,16 +1,22 @@
 const { Router } = require('express');
 const UserAdmin = require('./AdminRoutes');
 const usersRouter = require('./users.js');
-const categoryRoute = require('./CategoryRoute.js');
+const categoriesRouter = require('./CategoryRoute.js');
 const productRouter = require('./ProductRoute.js');
 const catalogRouter = require('./catalog.js');
+const seasonsRouter = require('./seasons.js');
 
 const router = Router();
 
 router.use('/users', usersRouter);
 router.use('/admin', UserAdmin);
 router.use('/products', productRouter);
-router.use('/category', categoryRoute);
-router.use('/catalog', catalogRouter); //! temporalmente
+router.use('/catalog', catalogRouter); // temporal
+router.use('/categories', categoriesRouter);
+router.use('/seasons', seasonsRouter);
+// router.use('/favorites', categoryRouter);
+// router.use('/cart', cartRouter);
+// router.use('/orders', ordersRouter);
+// router.use('/reviews', reviewsRouter);
 
 module.exports = router;
