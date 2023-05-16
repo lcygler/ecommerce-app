599727fec5fcb2ec4945a563b898fd4e48bdbc51
DieFleitas <diegofleitas98@outlook.es>
2023-05-16 00:12:01 -0300

    vinculate cart route to index router

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,6 +3,7 @@ const { Router } = require('express');
 const usersRouter = require('./users.js');
 const catalogRouter = require('./catalog.js');
 const productRouter = require('./product.js');
+const cartRouter = require('./cart.js');
 
 const router = Router();
 
@@ -11,6 +12,8 @@ router.use('/users', usersRouter);
 
 router.use('/catalog', catalogRouter);
 
-router.use("/products", productRouter)
+router.use("/products", productRouter);
+
+router.use("/cart", cartRouter)
 
 module.exports = router;
