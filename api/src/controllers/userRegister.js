a4ece4e6186fa6dd396bbbcb6fbb35db5f40fde8
ArrayagoM <juanmartinarrayago@gmail.com>
2023-05-23 18:07:54 -0300

    implementación de email

--- api/src/controllers/userRegister.js
+++ api/src/controllers/userRegister.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const { User } = require('../db.js');
 const { encrypt } = require('../utils/HashPassword.js');
+const { sendEmail } = require('../utils/mail.config.js')
 
 const registerCtrl = async (
   name,
@@ -34,7 +35,7 @@ const registerCtrl = async (
 
   // Generar JWT token
   const token = jwt.sign({ id: createUser.id }, process.env.JWT_SECRET, { expiresIn: '1d' });
-
+  sendEmail(email);
   return { user: createUser, token };
 };
 
