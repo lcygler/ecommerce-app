45b41de972deb32c1b75150a8974fdb21b4e686a
lcygler <leandrocygler@gmail.com>
2023-05-23 17:26:17 -0300

    api/routes: combinación de rutas y controllers

--- api/src/routes/genders.js
+++ api/src/routes/genders.js
@@ -1,8 +1,18 @@
 const { Router } = require('express');
-const getGendersHandler = require('../handlers/getGendersHandler.js');
 
 const router = Router();
 
-router.get('/', getGendersHandler);
+router.get('/', async (req, res) => {
+  try {
+    const genders = [
+      { id: 1, name: 'Hombre' },
+      { id: 2, name: 'Mujer' },
+      { id: 3, name: 'Otros' },
+    ];
+    res.status(200).json(genders);
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+});
 
 module.exports = router;
--- api/src/routes/seasons.js
+++ api/src/routes/seasons.js
@@ -1,8 +1,19 @@
 const { Router } = require('express');
-const getSeasonsHandler = require('../handlers/getSeasonsHandler.js');
 
 const router = Router();
 
-router.get('/', getSeasonsHandler);
+router.get('/', async (req, res) => {
+  try {
+    const seasons = [
+      { id: 1, name: 'Verano' },
+      { id: 2, name: 'Primavera' },
+      { id: 3, name: 'Invierno' },
+      { id: 4, name: 'Otoño' },
+    ];
+    res.status(200).json(seasons);
+  } catch (error) {
+    res.status(500).json({ error: 'Server error' });
+  }
+});
 
 module.exports = router;
