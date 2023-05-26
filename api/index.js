39cf59941be22e201a0e7c9971884da365d98280
Leandro Cygler <leandrocygler@gmail.com>
2023-05-25 21:39:06 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const { conn } = require('./src/db.js');
 const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
-conn.sync({ force: true  }).then(async () => {
+conn.sync({ force: false  }).then(async () => {
   await AddProducts(api);
   server.listen(port, () => {
     console.log(`%s listening at ${port}`); // eslint-disable-line no-console
