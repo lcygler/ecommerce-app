fbca43e767fac9dad7be1471a4217a761ced9053
lcygler <leandrocygler@gmail.com>
2023-05-30 19:27:16 -0300

    client: add search bar

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,7 +1,55 @@
-import { Box } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useLocation } from 'react-router-dom';
+import { actions } from '../redux/slice';
+
+import { Flex, Input /* Button */ } from '@chakra-ui/react';
 
 function SearchBar() {
-  return <Box></Box>;
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    if (location.pathname === '/home') {
+      dispatch(actions.filterProducts(searchTerm));
+    } else if (location.pathname === '/favorites') {
+      dispatch(actions.filterFavorites(searchTerm));
+    } else if (location.pathname === '/dashboard') {
+      dispatch(actions.filterAdminProducts(searchTerm));
+    }
+  }, [dispatch, searchTerm, location]);
+
+  // const handleSearch = () => {
+  //   dispatch(actions.filterProducts(searchTerm));
+  // };
+
+  // const handleKeyPress = (event) => {
+  //   if (event.key === 'Enter') {
+  //     handleSearch();
+  //   }
+  // };
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <Flex align="center" justify="center" /* mb={4} w="100%" */>
+      <Input
+        type="text"
+        placeholder="Search..."
+        value={searchTerm}
+        onChange={handleChange}
+        // onKeyPress={handleKeyPress}
+        // mr={2}
+      />
+
+      {/* <Button onClick={handleSearch} colorScheme="blue">
+        Search
+      </Button> */}
+    </Flex>
+  );
 }
 
 export default SearchBar;
