6b66169bdcbae5fd1b89f7eb68bde7f95f36cc67
lcygler <leandrocygler@gmail.com>
2023-05-30 13:46:51 -0300

    client: update detail

--- client/src/components/ReviewCard.jsx
+++ client/src/components/ReviewCard.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
 import { Box, Text } from '@chakra-ui/react';
-import { StarRating } from '../components';
+import React from 'react';
+import { StarRating } from '.';
 
 function ReviewCard({ punctuation, comment }) {
   return (
     <Box bg="white" borderRadius="md" p="4" mb="4" boxShadow="md" width="50%" mx="auto">
       <Box color="blue.500" fontWeight="bold" fontSize="lg" mb="2">
-        <StarRating value={punctuation}/>
+        <StarRating value={punctuation} />
       </Box>
       <Text color="gray.600" fontSize="sm">
         {comment}
