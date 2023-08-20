37e4c46d6771da58e45b5847f88666400f30a73b
Leandro Cygler <leandrocygler@gmail.com>
2023-08-20 12:34:17 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const { conn } = require('./src/db.js');
 const port = process.env.PORT || 3001;
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   AddProducts(products);
   saveUsersToDatabase();
   addNewUserPeriodically();
