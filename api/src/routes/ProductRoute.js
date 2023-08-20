f1b6476b1ff5eabab132d269b42e007bad496fcc
lcygler <leandrocygler@gmail.com>
2023-08-20 03:02:53 -0300

    -

--- api/src/routes/ProductRoute.js
+++ api/src/routes/ProductRoute.js
@@ -16,7 +16,8 @@ router.get('/', async (req, res) => {
     res.status(200).json(products);
   } catch (error) {
     console.log(error);
-    res.status(500).json({ error: 'Server error' });
+    // res.status(500).json({ error: 'Server error' });
+    res.status(500).json({ error: error });
   }
 });
 
